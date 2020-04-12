17590ec128f5fe34f87b87ac8afc129883dca3be
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-04-12 21:03:07 +0530

    generating random hex color instead of predefined set of colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,11 @@
 const button = document.querySelector('button');
 const body = document.querySelector('body');
 
-const colors = ['pink', 'green', 'yellow', 'teal', 'grey'];
-//                0       1         2        3         4
 body.style.backgroundColor = 'violet';
 
 button.addEventListener('click', changeBackground);
 
 function changeBackground() {
-  const colorIndex = Math.floor(Math.random() * colors.length);
-  body.style.backgroundColor = colors[colorIndex];
+  const colorIndex = Math.floor(Math.random() * 16777216);
+  body.style.backgroundColor = '#' + colorIndex.toString(16);
 }
